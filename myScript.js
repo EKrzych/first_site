5a830c953507274cc5d07e43a9f5368c3062cb03
Elzbieta Krzych <elzbieta.krzych@gmail.com>
2018-06-18 01:42:48 +0200

    Add subsites

--- myScript.js
+++ myScript.js
@@ -1,8 +1,8 @@
-function validate() {
+function validateIfEmpty() {
     var isValid=0;
-    isValid+=validateFirstName();
-    isValid+=validateEmail();
-    isValid+=validateTextArea();
+    isValid+=validateFirstNameEmpty();
+    isValid+=validateEmailEmpty();
+    isValid+=validateTextAreaEmpty();
     
     if(isValid==3) {
         document.getElementById("submit").disabled=false;
@@ -11,30 +11,60 @@ function validate() {
     }
 }
 
+function validate() {
+    validateIfEmpty();
+    validateFirstName();
+    validateEmail();
+    validateTextArea();
+}
+
 function validateFirstName() {
     var firstName = document.getElementById("firstName");
     var pattern = /(.?[A-Z][a-z]*)+/;
     if(pattern.test(firstName.value)){
+        document.getElementById("errorMessageFirstName").className="error-hidden";
         return 1;
     } else {
-        firstName.class="error-visible";
+        document.getElementById("errorMessageFirstName").className="error-visible"; 
     }
 }
 
 function validateEmail() {
     var email = document.getElementById("email");
     var pattern =/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}/;
-    if(email.value != ""){
+    if(pattern.test(email.value)) {
+        document.getElementById("errorMessageEmail").className="error-hidden";
         return 1;
     } else {
-        email.class="error-visible";
+        document.getElementById("errorMessageEmail").className="error-visible";
+    }
+}
+
+function validateFirstNameEmpty() {
+    var firstName = document.getElementById("firstName");
+    if(firstName.value != ""){
+        return 1;
+    }
+}
+
+function validateEmailEmpty() {
+    var email = document.getElementById("email");
+    if(email.value != ""){
+        return 1;
+    }
+}
+
+function validateTextAreaEmpty() {
+    if(document.getElementById("message").value != ""){
+        return 1;
     }
 }
 
 function validateTextArea() {
     if(document.getElementById("message").value != ""){
+        document.getElementById("errorMessage").className="error-hidden";
         return 1;
     } else {
-        document.getElementById("message").class="error-visible";
+        document.getElementById("errorMessage").className="error-visible";
     }
 }
